a7b3aa571c0663e41cc952dc9ee50dbb36d80f4b
LuiGGi629 <wojtek.felix.zuber@gmail.com>
2020-09-15 18:36:08 +0200

    Composition to Change Run-Time Behavior

--- program.py
+++ program.py
@@ -1,17 +1,12 @@
-from hr import calculate_payroll
-import json
+from hr import LTDPolicy, calculate_payroll
 from productivity import track
-from employees import Employee, employee_database
-
-
-def print_dict(d):
-    print(json.dumps(d, indent=2))
-
+from employees import employee_database
 
 employees = employee_database.employees
+
+sales_employee = employees[2]
+ltd_policy = LTDPolicy()
+sales_employee.apply_payroll_policy(ltd_policy)
+
 track(employees, 40)
 calculate_payroll(employees)
-
-temp_secretary = Employee(5)
-print("Temporary Secretary:")
-print_dict(temp_secretary.to_dict())
